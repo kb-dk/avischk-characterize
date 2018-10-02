8b6b278564b255432d409bd50395810ae963be35
ktc <ktc@statsbiblioteket.dk>
2018-10-02 11:40:11 +0200

    change sql to only take files of a certain type for a given newspaper that has not already been characterized by the same tool

--- characterize.py
+++ characterize.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python2.6
+# -*- coding: utf-8 -*-
 
 import ConfigParser
 import os
+#import io
+import StringIO
 import psycopg2
 import datetime
 import sys
 import subprocess
-
+from lxml import etree
 
 config = ConfigParser.RawConfigParser()
 config.read("avischk-characterize-config.cfg")
@@ -17,14 +20,15 @@ dbhost = config.get("db-config", "host")
 dbpassword = config.get("db-config", "password")
 
 
-def getFilesToCharacterize(avisid, format_type, already_characterised):
-    sql = """SELECT orig_relpath FROM newspaperarchive WHERE avisid = %s AND format_type = %s limit 2;"""
+def getFilesToCharacterize(avisid, format_type, tool):
+    sql = """SELECT orig_relpath FROM (SELECT orig_relpath, tool FROM newspaperarchive LEFT JOIN characterisation_info USING (orig_relpath) 
+             WHERE characterisation_info.orig_relpath IS NULL AND avisid = %s AND format_type = %s) AS foo WHERE foo.tool IS NULL OR foo.tool != %s"""
     files = []
     conn = None
     try:
         conn = psycopg2.connect(host=dbhost, database=dbname, user=dbuser, password=dbpassword)
         cursor = conn.cursor()
-        cursor.execute(sql, (avisid, format_type))
+        cursor.execute(sql, (avisid, format_type, tool))
         res = cursor.fetchall()
         for row in res: 
             files.append(row[0])
@@ -39,15 +43,15 @@ def getFilesToCharacterize(avisid, format_type, already_characterised):
     return files
 
 
-def storeInDB(orig_relpath, tool, date, tool_output, status):
-    sql = """INSERT INTO characterisation_info(orig_relpath, tool, date, tool_output, status) VALUES
-            (%s, %s, %s, %s, %s);"""
+def storeInDB(orig_relpath, tool, tool_output, status):
+    sql = """INSERT INTO characterisation_info(orig_relpath, tool, characterisation_date, tool_output, status) VALUES
+            (%s, %s, now(), %s, %s);"""
 
     conn = None
     try:
         conn = psycopg2.connect(host=dbhost, database=dbname, user=dbuser, password=dbpassword)
         cursor = conn.cursor()
-        cursor.execute(sql, (orig_relpath, tool, date, tool_output, status))
+        cursor.execute(sql, (orig_relpath, tool, tool_output, status))
         conn.commit()
         cursor.close()
     except (Exception) as error:
@@ -56,18 +60,52 @@ def storeInDB(orig_relpath, tool, date, tool_output, status):
         if conn is not None:
             conn.close()
 
+def run_jpylyzer(filePath):
+    output = []
+    characterize_command = ['jpylyzer', filePath]
+    try:
+        exProc = subprocess.Popen(characterize_command, bufsize=-1, stdout=subprocess.PIPE)
+        stdout = exProc.stdout
+        for line in stdout:
+            output.append(line)
+
+    except (subprocess.CalledProcessError) as err:
+        print err
+
+    return output
+
+def validate_jpylyzer_characterization(output):
+    schematronFile = open("sb-jp2.sch", "r")
+    sct_doc = etree.parse(schematronFile)
+    schematron = etree.Schematron(sct_doc)
+
+    schematronFile.close()
+    xml = StringIO.StringIO(output)
+
+    doc = etree.parse(xml)
+    valid = schematron.validate(doc)
+    
+    if valid:
+        return "valid"
+    else:
+        return "invalid"
+    
 
 def run_characterize():
-    files = getFilesToCharacterize('boersen', 'jp2', False)
+    tool = 'jpylyzer'
+    files = getFilesToCharacterize('boersen', 'jp2', tool)
     
     for f in files:
         filePath = '/digitape/samba-links/Avisarkiver_eksterne/' + f
 
-        characterize_command = 'jpylyzer ' + filePath
+        out = run_jpylyzer(filePath)
+        outstr = unicode("".join(out), 'utf-8')
+        status = validate_jpylyzer_characterization(outstr)
 
-        (status, output) = subprocess.getstatusoutput(charcterize_command)
+        print f
+        print status
+        storeInDB(f, tool, outstr, status)
 
-        print "   ", output
 
 
 if __name__ == '__main__':
